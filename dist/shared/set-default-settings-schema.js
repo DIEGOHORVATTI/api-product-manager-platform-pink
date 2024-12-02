23c50a1d1c2e92edee85cd4b12f3566c6e4f3308
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-01 22:56:36 -0300

    Remove unused files and update Vercel configuration for improved deployment

--- dist/shared/set-default-settings-schema.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"setDefaultSettingsSchema",{enumerable:true,get:function(){return setDefaultSettingsSchema}});const setDefaultSettingsSchema=schema=>{schema.set("toJSON",{virtuals:true});schema.set("toObject",{virtuals:true});schema.set("versionKey",false)};
