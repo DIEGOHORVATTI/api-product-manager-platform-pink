b78f2379695135c8bedbd76d7e564a0cc7fae59a
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-01 23:34:44 -0300

    Remove MongoDB environment variables from type definitions

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,17 +4,6 @@ export declare global {
       // string(development | production), example: development
       NODE_ENV: 'development' | 'production'
 
-      // string, example: banco_of_datas
-      MONGODB_USERNAME: string
-      // string, example: 0000
-      MONGODB_PASSWORD: number
-      // number, example: 27017
-      MONGODB_PORT: number
-      // string, example: project
-      MONGODB_DATABASE: string
-      // string, example: localhost
-      MONGO_DOMAIN: string
-
       // string, example: 90d0b2f0-4b1a-4b1a-8b1a-4b1a4b1a4b1a
       JWT_SECRET: string
       // string, example 2 | 10 | 30 days
